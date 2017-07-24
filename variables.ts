4cb60399aa07ffb2b635d44746024a4671d249d6
york yao <yaoao12306@outlook.com>
2017-07-24 21:26:47 +0800

    improve generated json schema

--- variables.ts
+++ variables.ts
@@ -2,6 +2,7 @@
 export const generateMatchesTemplateHtml = `<div class="generate-matches"><textarea v-model="text" spellcheck="false"></textarea><div v-if="errorMessage" class="error-message">{{errorMessage}}</div><button @click="generate()">Generate</button><textarea v-model="result"></textarea></div>`;
 export const indexTemplateHtml = `<div class="main"><textarea v-model="text" spellcheck="false"></textarea><div v-if="errorMessage" class="error-message">{{errorMessage}}</div><button @click="calculate()">Calculate</button><div class="result"><table v-for="(group, i) in result"><thead><tr><th>group {{i + 1}}</th><th v-for="top in group.tops">top {{top}} chance(%)</th><th>score</th><th>match count left</th></tr></thead><tbody><tr v-for="team in group.chances"><td>{{team.name}}</td><td v-for="chance in team.chances">{{chance}}</td><td>{{team.score}}</td><td>{{team.matchCountLeft}}</td></tr></tbody></table></div></div>`;
 export const groupsSchemaJson = {
+  "$ref": "#/definitions/Groups",
   "definitions": {
     "Groups": {
       "type": "array",
@@ -88,88 +89,11 @@ export const groupsSchemaJson = {
       "uniqueItems": true,
       "minItems": 1
     }
-  },
-  "$ref": "#/definitions/Groups"
+  }
 };
 export const teamsSchemaJson = {
+  "$ref": "#/definitions/Teams",
   "definitions": {
-    "Groups": {
-      "type": "array",
-      "items": {
-        "$ref": "#/definitions/Group"
-      },
-      "uniqueItems": true,
-      "minItems": 1
-    },
-    "Group": {
-      "type": "object",
-      "properties": {
-        "matches": {
-          "type": "array",
-          "items": {
-            "$ref": "#/definitions/Match"
-          },
-          "uniqueItems": true,
-          "minItems": 1
-        },
-        "teams": {
-          "$ref": "#/definitions/Teams"
-        },
-        "tops": {
-          "type": "array",
-          "items": {
-            "type": "integer",
-            "minimum": 1
-          },
-          "uniqueItems": true,
-          "minItems": 1
-        }
-      },
-      "required": [
-        "matches",
-        "teams",
-        "tops"
-      ],
-      "additionalProperties": false
-    },
-    "Match": {
-      "type": "object",
-      "properties": {
-        "a": {
-          "type": "string"
-        },
-        "b": {
-          "type": "string"
-        },
-        "possibilities": {
-          "type": "array",
-          "items": {
-            "type": "object",
-            "properties": {
-              "a": {
-                "type": "integer"
-              },
-              "b": {
-                "type": "integer"
-              }
-            },
-            "required": [
-              "a",
-              "b"
-            ],
-            "additionalProperties": false
-          },
-          "uniqueItems": true,
-          "minItems": 1
-        }
-      },
-      "required": [
-        "a",
-        "b",
-        "possibilities"
-      ],
-      "additionalProperties": false
-    },
     "Teams": {
       "type": "array",
       "items": {
@@ -178,7 +102,6 @@ export const teamsSchemaJson = {
       "uniqueItems": true,
       "minItems": 1
     }
-  },
-  "$ref": "#/definitions/Teams"
+  }
 };
 // tslint:enable:object-literal-key-quotes trailing-comma
