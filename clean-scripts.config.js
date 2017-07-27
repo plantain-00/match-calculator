a5eb63e580cb13255579e3119e5badb04fa485a4
york yao <yaoao12306@outlook.com>
2017-07-28 00:14:31 +0800

    clean scripts

---
+++ clean-scripts.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  build: [
+    `rimraf index.bundle-*.js index.bundle-*.css`,
+    `types-as-schema types.ts --json .`,
+    `file2variable-cli *.template.html *-schema.json -o variables.ts --html-minify --json`,
+    `tsc`,
+    `cleancss index.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css ./node_modules/tab-container-component/dist/tab-container.min.css -o index.bundle.css`,
+    `webpack`,
+    `rev-statics`,
+    `sw-precache --config sw-precache.config.js`,
+    `uglifyjs service-worker.js -o service-worker.bundle.js`
+  ],
+  lint: [
+    `tslint "*.ts"`,
+    `standard "**/*.config.js"`
+  ],
+  test: [
+    'tsc -p spec',
+    'karma start spec/karma.config.js'
+  ],
+  fix: [
+    `standard --fix "**/*.config.js"`
+  ]
+}
