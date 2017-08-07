57afd6f735c5d6ee3a3bf465d8c2621924004922
york yao <yaoao12306@outlook.com>
2017-08-07 23:09:56 +0800

    improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,33 +1,46 @@
 module.exports = {
   build: [
-    `rimraf index.bundle-*.js vendor.bundle-*.js index.bundle-*.css`,
-    `cpy node_modules/monaco-editor/min/vs/loader.js vs/`,
-    `cpy node_modules/monaco-editor/min/vs/language/json/jsonMode.js vs/language/json/`,
-    `cpy node_modules/monaco-editor/min/vs/language/json/jsonWorker.js vs/language/json/`,
-    `cpy node_modules/monaco-editor/min/vs/editor/editor.main.js vs/editor/`,
-    `cpy node_modules/monaco-editor/min/vs/editor/editor.main.css vs/editor/`,
-    `cpy node_modules/monaco-editor/min/vs/editor/editor.main.nls.js vs/editor/`,
-    `cpy node_modules/monaco-editor/min/vs/base/worker/workerMain.js vs/base/worker/`,
-    `types-as-schema types.ts --json .`,
-    `file2variable-cli *.template.html *-schema.json -o variables.ts --html-minify --json`,
-    `tsc`,
-    `lessc index.less > index.css`,
-    `cleancss index.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css ./node_modules/tab-container-component/tab-container.min.css -o index.bundle.css`,
-    `webpack --display-modules`,
-    `rev-static`,
+    {
+      version: [
+        {
+          js: [
+            `types-as-schema types.ts --json .`,
+            `file2variable-cli *.template.html *-schema.json -o variables.ts --html-minify --json`,
+            `tsc`,
+            `webpack --display-modules`
+          ],
+          css: [
+            `lessc index.less > index.css`,
+            `cleancss index.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css ./node_modules/tab-container-component/tab-container.min.css -o index.bundle.css`
+          ],
+          clean: `rimraf index.bundle-*.js vendor.bundle-*.js index.bundle-*.css`
+        },
+        `rev-static`
+      ],
+      copy: [
+        `cpy node_modules/monaco-editor/min/vs/loader.js vs/`,
+        `cpy node_modules/monaco-editor/min/vs/language/json/jsonMode.js vs/language/json/`,
+        `cpy node_modules/monaco-editor/min/vs/language/json/jsonWorker.js vs/language/json/`,
+        `cpy node_modules/monaco-editor/min/vs/editor/editor.main.js vs/editor/`,
+        `cpy node_modules/monaco-editor/min/vs/editor/editor.main.css vs/editor/`,
+        `cpy node_modules/monaco-editor/min/vs/editor/editor.main.nls.js vs/editor/`,
+        `cpy node_modules/monaco-editor/min/vs/base/worker/workerMain.js vs/base/worker/`
+      ]
+    },
     `sw-precache --config sw-precache.config.js`,
     `uglifyjs service-worker.js -o service-worker.bundle.js`
   ],
-  lint: [
-    `tslint "*.ts"`,
-    `standard "**/*.config.js"`,
-    `stylelint "/*.less"`
-  ],
+  lint: {
+    ts: `tslint "*.ts"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "/*.less"`
+  },
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js'
   ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ]
+  fix: {
+    js: `standard --fix "**/*.config.js"`,
+    less: `stylelint --fix "/*.less"`
+  }
 }
